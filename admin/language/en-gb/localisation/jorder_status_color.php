28829b2eca8acefa2c5edf0a96142e969a827ae5
LAN Studio <a20034294@gmail.com>
2020-04-25 01:06:21 +0800

    Delete en-gb language

--- admin/language/en-gb/localisation/jorder_status_color.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Column
-$_['column_backgrond_color']      = 'Background Color';
-$_['column_text_color']      = 'Text Color';
-
-// Entry
-$_['entry_backgrond_color']      = 'Background Color';
-$_['entry_text_color']      = 'Text Color';
