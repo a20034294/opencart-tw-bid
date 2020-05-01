4143614b0d01b98e4b575facc2fdaa8cc586120b
LAN Studio <a20034294@gmail.com>
2020-05-01 21:45:29 +0800

    Revert "Delete en-gb language"
    This reverts commit 28829b2eca8acefa2c5edf0a96142e969a827ae5.

---
+++ admin/language/en-gb/localisation/jorder_status_color.php
@@ -0,0 +1,8 @@
+<?php
+// Column
+$_['column_backgrond_color']      = 'Background Color';
+$_['column_text_color']      = 'Text Color';
+
+// Entry
+$_['entry_backgrond_color']      = 'Background Color';
+$_['entry_text_color']      = 'Text Color';
